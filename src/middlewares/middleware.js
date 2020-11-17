91ca142ce6b45e4aeb280bda7c25603a68fa0bea
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2020-11-17 17:38:47 -0300

    ajuste de msg de erro

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -25,10 +25,11 @@ exports.checkCsrfError = (err, req, res, next) => {
     if (err && 'EBADCSRFTOKEN' === err.code) {
         res.render('404');
     }
+    next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
 
-    next()
+    next();
 }
